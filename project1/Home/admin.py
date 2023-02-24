73197e092dc05d1dd5ce642fc3c886e87a31e574
shubham goel <thorgoyal@gmail.com>
2023-02-24 11:08:07 +0530

    initail commit

---
+++ project1/Home/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from Home.models import WebBeta1, WebBeta2, WebBeta3,WebBeta4,NewUpdateInfo
+# Register your models here.
+
+admin.site.register(WebBeta1)
+admin.site.register(WebBeta2)
+admin.site.register(WebBeta3)
+admin.site.register(WebBeta4)
+admin.site.register(NewUpdateInfo)
